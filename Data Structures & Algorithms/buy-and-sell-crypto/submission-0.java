0dffe35e57bc3d77e9281c141ef9102bcf968751
sohaibbadaha121 <1210472@student.birzeit.edu>
2026-05-26 15:19:04 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0 ;
+        for(int i = 0 ; i<prices.length ; i++){
+            for(int j= i+1 ; j<prices.length ; j ++){
+               int cur= prices[j]-prices[i];
+                if(cur>max){
+                    max=cur;
+                }
+            }
+        }
+        return max;
+    }
+}
