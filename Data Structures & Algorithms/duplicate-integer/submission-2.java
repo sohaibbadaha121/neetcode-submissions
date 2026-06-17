f9bd7258341a62a2b7ad46f7a06facf7f327f1b7
sohaibbadaha121 <1210472@student.birzeit.edu>
2026-06-17 20:20:55 +0100

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+      Arrays.sort(nums);
+      for(int i = nums.length-1 ; i >0 ; i--){
+        if(nums[i]==nums[i-1]){
+            return true;
+        }
+      }
+      return false;
+}}
